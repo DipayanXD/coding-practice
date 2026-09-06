064d0df237417c0f678710f3fda71784abbe4940
Dipayan Das <dipayan029@gmail.com>
2026-09-06 13:41:25 +0530

    Create first.php

---
+++ php/first.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Hello Dipayan <br>";
+    echo bcpow(100,100);
+    $range = 100;
+    $num = 2;
+    for($i=1;$i<=$range;$i++){
+        echo "{$num} * {$i} = " . ($num * $i) . "<br>";
+    }
+        
+?>
